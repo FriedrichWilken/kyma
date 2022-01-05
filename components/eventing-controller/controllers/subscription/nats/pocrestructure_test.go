dc33ad1fb02bd0d50e5654ff52c5867c5cd0f7a7
Friedrich Wilken <j.wilken@posteo.net>
2022-01-05 11:09:30 +0100

    restructure nats test

---
+++ components/eventing-controller/controllers/subscription/nats/pocrestructure_test.go
@@ -0,0 +1,81 @@
+package nats
+
+import (
+	"fmt"
+	"context"
+	v1 "k8s.io/api/core/v1"
+	eventingv1alpha1 "github.com/kyma-project/kyma/components/eventing-controller/api/v1alpha1"
+	reconcilertesting "github.com/kyma-project/kyma/components/eventing-controller/testing"
+	. "github.com/onsi/ginkgo"
+
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Subscription Reconciliation Tests", func() {
+	var id int
+
+	testCases := []struct {
+		context, eventTypePrefix, natsSubjectToPublish, eventTypeToSubscribe string
+	}{
+		{
+			context:              "NATS Subscription reconciler tests with non-empty eventTypePrefix",
+			eventTypePrefix:      reconcilertesting.EventTypePrefix,
+			natsSubjectToPublish: reconcilertesting.OrderCreatedEventType,
+			eventTypeToSubscribe: reconcilertesting.OrderCreatedEventTypeNotClean,
+		},
+		{
+			context:              "NATS Subscription reconciler tests with empty eventTypePrefix",
+			eventTypePrefix:      reconcilertesting.EventTypePrefixEmpty,
+			natsSubjectToPublish: reconcilertesting.OrderCreatedEventTypePrefixEmpty,
+			eventTypeToSubscribe: reconcilertesting.OrderCreatedEventTypeNotCleanPrefixEmpty,
+		},
+	}
+	BeforeEach(func() {
+		id++
+	})
+
+	for _, testCase := range testCases {
+		Context(testCase.context, func() {
+			When("Create/Delete Subscription", func() {
+				It("Should create/delete NATS Subscription", func() {
+					ctx := context.Background()
+					cancel = startReconciler(testCase.eventTypePrefix, defaultSinkValidator)
+					defer cancel()
+					subscriptionName := fmt.Sprintf(subscriptionNameFormat, id)
+					subscriberName := fmt.Sprintf(subscriberNameFormat, id)
+
+					// create subscriber svc
+					subscriberSvc := reconcilertesting.NewSubscriberSvc(subscriberName, namespaceName)
+					ensureSubscriberSvcCreated(ctx, subscriberSvc)
+
+					// create subscription
+					optFilter := reconcilertesting.WithFilter(reconcilertesting.EventSource, testCase.eventTypeToSubscribe)
+					optWebhook := reconcilertesting.WithWebhookForNats
+					subscription := reconcilertesting.NewSubscription(subscriptionName, namespaceName, optFilter, optWebhook)
+					reconcilertesting.WithValidSink(namespaceName, subscriberSvc.Name, subscription)
+					ensureSubscriptionCreated(ctx, subscription)
+
+					getSubscription(ctx, subscription).Should(And(
+						reconcilertesting.HaveSubscriptionName(subscriptionName),
+						reconcilertesting.HaveCondition(eventingv1alpha1.MakeCondition(
+							eventingv1alpha1.ConditionSubscriptionActive,
+							eventingv1alpha1.ConditionReasonNATSSubscriptionActive,
+							v1.ConditionTrue, "")),
+						reconcilertesting.HaveSubsConfiguration(&eventingv1alpha1.SubscriptionConfig{
+							MaxInFlightMessages: defaultSubsConfig.MaxInFlightMessages,
+						}),
+					))
+
+					// check for subscription at nats
+					backendSubscription := getSubscriptionFromNats(natsBackend.GetAllSubscriptions(), subscriptionName)
+					Expect(backendSubscription).NotTo(BeNil())
+					Expect(backendSubscription.IsValid()).To(BeTrue())
+					Expect(backendSubscription.Subject).Should(Equal(testCase.natsSubjectToPublish))
+
+					Expect(k8sClient.Delete(ctx, subscription)).Should(BeNil())
+					isSubscriptionDeleted(ctx, subscription).Should(reconcilertesting.HaveNotFoundSubscription(true))
+				})
+			})
+		})
+	}
+})
